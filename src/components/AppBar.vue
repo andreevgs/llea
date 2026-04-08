f669e6eef478082d5961f8f761bc0166a93aa28b
Gleb Andreev <andreevgs.dev@yahoo.com>
2026-04-08 21:14:08 +0200

    feat: add global scroll, style configurations and fixes

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -3,71 +3,74 @@
     class="position-fixed top-0 left-0 border-b"
     flat
   >
-    <v-app-bar-title> LLEA </v-app-bar-title>
-    <v-spacer />
-    <v-btn
-      class="mr-2"
-      prepend-icon="mdi-chart-box"
-      to="/progress"
-    >
-      {{ $t("app_bar.progress") }}
-    </v-btn>
-    <v-btn
-      class="mr-2"
-      prepend-icon="mdi-list-box-outline"
-      to="/essays"
-    >
-      {{ $t("app_bar.essays") }}
-    </v-btn>
-    <v-btn
-      class="mr-2"
-      prepend-icon="mdi-book-alphabet"
-      to="/dictionary"
-    >
-      {{ $t("app_bar.dictionary") }}
-    </v-btn>
-    <v-tooltip
-      location="bottom"
-      :text="toggleThemeButtonTooltip"
-    >
-      <template #activator="{ props }">
-        <v-btn
-          v-bind="props"
-          class="mr-2"
-          icon="mdi-theme-light-dark"
-          @click="toggleTheme()"
-        />
-      </template>
-    </v-tooltip>
-    <v-btn
-      :append-icon="`$flag-${languagesStore.targetLanguage}`"
-      class="mr-2"
-      color="primary"
-      :prepend-icon="`$flag-${languagesStore.currentLanguage}`"
-      variant="tonal"
-      @click="isChangeLanguagesModalOpen = true"
-    >
-      /
+    <template #prepend>
+      <v-app-bar-title class="ps-2 ps-md-5"> LLEA </v-app-bar-title>
+    </template>
+    <template #append>
+      <v-btn
+        class="mr-2"
+        prepend-icon="mdi-chart-box"
+        to="/progress"
+      >
+        {{ $t("app_bar.progress") }}
+      </v-btn>
+      <v-btn
+        class="mr-2"
+        prepend-icon="mdi-list-box-outline"
+        to="/essays"
+      >
+        {{ $t("app_bar.essays") }}
+      </v-btn>
+      <v-btn
+        class="mr-2"
+        prepend-icon="mdi-book-alphabet"
+        to="/dictionary"
+      >
+        {{ $t("app_bar.dictionary") }}
+      </v-btn>
       <v-tooltip
-        activator="parent"
         location="bottom"
-      >{{ $t("app_bar.switch_languages") }}
+        :text="toggleThemeButtonTooltip"
+      >
+        <template #activator="{ props }">
+          <v-btn
+            v-bind="props"
+            class="mr-2"
+            icon="mdi-theme-light-dark"
+            @click="toggleTheme()"
+          />
+        </template>
       </v-tooltip>
-    </v-btn>
-    <v-divider
-      inset
-      vertical
-    />
-    <v-btn
-      class="mr-2 ml-2"
-      color="primary"
-      prepend-icon="mdi-plus"
-      to="/new-essay"
-      variant="tonal"
-    >
-      {{ $t("app_bar.new_essay") }}
-    </v-btn>
-    <change-languages-dialog v-model="isChangeLanguagesModalOpen" />
+      <v-btn
+        :append-icon="`$flag-${languagesStore.targetLanguage}`"
+        class="mr-2"
+        color="primary"
+        :prepend-icon="`$flag-${languagesStore.currentLanguage}`"
+        variant="tonal"
+        @click="isChangeLanguagesModalOpen = true"
+      >
+        /
+        <v-tooltip
+          activator="parent"
+          location="bottom"
+        >{{ $t("app_bar.switch_languages") }}
+        </v-tooltip>
+      </v-btn>
+      <v-divider
+        inset
+        vertical
+      />
+      <v-btn
+        class="mr-2 ml-2"
+        color="primary"
+        prepend-icon="mdi-plus"
+        to="/new-essay"
+        variant="tonal"
+      >
+        {{ $t("app_bar.new_essay") }}
+      </v-btn>
+      <change-languages-dialog v-model="isChangeLanguagesModalOpen" />
+    </template>
   </v-app-bar>
 </template>
 <script setup lang="ts">
